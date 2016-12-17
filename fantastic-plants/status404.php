3f286ca2eb0ede9534f289b5b8d720b37d9576cf
Amelia Růžek Klein <aeruzek@gmail.com>
2016-12-17 16:51:35 -0600

    init add
    all files from final project

---
+++ fantastic-plants/status404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>404 Error | Fantastic Plants | No Man's Sky</title>
+        <meta charset="utf-8" />
+        <meta name=viewport content="width=device-width, initial-scale=1" />
+
+        <!--Add Favicon-->
+        <link rel="icon" href="images/favicon.png" type="image/gif" sizes="16x16">
+
+        <!--Add the Sketeton CSS-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
+         <link rel="stylesheet" href="http://cs12students.dce.harvard.edu/~aruzek/A1_PumpKInSeaS0n/final-project/css/plants.css" />
+    </head>
+    <body id="Error404" class="body">
+        
+        <div class="container">
+        
+            <!--Add Header-->
+            <header class="row spotlight">
+                <div class="twelve columns">
+                <h1>404 ERROR</h1> 
+                <h3>Oops it looks like you're lost. Let's get you back to Fantastic Plants and Where to Find Them.</h3>
+                    <a href="http://cs12students.dce.harvard.edu/~aruzek/A1_PumpKInSeaS0n/final-project/">
+                        <button>Engage Hyperdrive</button>
+                    </a>
+                </div>
+            </header>
+            
+        </div>
+    </body>
+</html>
